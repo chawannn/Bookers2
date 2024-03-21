5f1f1914d96f9a883e7129f7240d98697696d55d
chawannn <158204549+chawannn@users.noreply.github.com>
2024-03-21 15:22:32 +0000

    2 2024/03/21

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
 
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def new
     @book = Book.new
   end
@@ -9,9 +11,13 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
     if @book.save
       flash[:notice] = "You have created book successfully."
-      redirect_to books_path
+      redirect_to book_path(@book.id)
     else
-      render :new
+      @books = Book.all
+      #@book = Book.new
+      @user = current_user
+      @users = User.all
+      render :index
     end
   end
 
@@ -34,9 +40,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    flash[:notice] = "You have updated book successfully."
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -50,6 +59,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body, :image)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
   end
 
 end
